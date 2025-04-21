52ab4351e0da1c3b5feb24e4a95c9eede503570e
ilyasbrahim520 <ilyasbrahim520@gmail.com>
2025-04-22 00:17:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -82,3 +82,10 @@ window.addEventListener("scroll", () => {
   }
   lastScroll = currentScroll;
 });
+
+// إضافة الشعار داخل رأس الصفحة
+const header = document.querySelector("header");
+const brandText = document.createElement("p");
+brandText.textContent = "Wallpapers by W-ilyas";
+brandText.className = "brand";
+header.appendChild(brandText);
