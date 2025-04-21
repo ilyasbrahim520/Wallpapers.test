e6a3bf50ff9ff2de703c5f261cf911e0cd0ab434
ilyasbrahim520 <ilyasbrahim520@gmail.com>
2025-04-21 19:39:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 const wallpapers = [
   {
-    thumb: "images/tesla-model-s-thumb.jpeg",
-    full: "images/tesla-model-s.jpeg",
-    alt: "Tesla Model S"
+    thumb: "images/pagani-zonda-r-thumb.jpeg",
+    full: "images/pagani-zonda-r.jpeg",
+    alt: "Pagani Zonda R"
   },
   {
-    thumb: "images/ferrari-f8-thumb.jpeg",
-    full: "images/ferrari-f8.jpeg",
-    alt: "Ferrari F8"
+    thumb: "images/porsche-911-front-thumb.jpeg",
+    full: "images/porsche-911-front.jpeg",
+    alt: "Sweet Porsche 911 front side"
   },
   {
-    thumb: "images/bmw-m4-thumb.jpeg",
-    full: "images/bmw-m4.jpeg",
-    alt: "BMW M4"
+    thumb: "images/supertrofeo-evo2-thumb.jpeg",
+    full: "images/supertrofeo-evo2.jpeg",
+    alt: "Candy SuperTrofeo EVO2"
   },
   {
-    thumb: "images/audi-r8-thumb.jpeg",
-    full: "images/audi-r8.jpeg",
-    alt: "Audi R8"
+    thumb: "images/porsche-911-left-thumb.jpeg",
+    full: "images/porsche-911-left.jpeg",
+    alt: "Sweet Porsche 911 left side"
   },
   {
-    thumb: "images/mercedes-amg-gt-thumb.jpeg",
-    full: "images/mercedes-amg-gt.jpeg",
-    alt: "Mercedes AMG GT"
+    thumb: "images/koenigsegg-agera-rs-thumb.jpeg",
+    full: "images/koenigsegg-agera-rs.jpeg",
+    alt: "Koenigsegg Agera RS"
   }
 ];
 
@@ -46,7 +46,7 @@ wallpapers.forEach(wallpaper => {
 
 const toggleButton = document.getElementById("toggleMode");
 
-// الوضع الافتراضي بناءً على تفضيل المستخدم (اختياري)
+// الوضع الافتراضي بناءً على تفضيل المستخدم
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
   document.body.classList.add("dark");
   toggleButton.textContent = "Light Mode";
